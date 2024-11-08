9cdc2ebc8ac724b1b2ff882a23fd46ac7dda7b49
Moksh Keloo <moksh@integratedwebworks.com>
2024-11-08 14:06:50 -0500

    fix: Restore original video source in BespokeAdsPage component

--- app/bespoke/page.tsx
+++ app/bespoke/page.tsx
@@ -9,8 +9,8 @@ const BespokeAdsPage = () => {
     <div>
       <h1>Bespoke Ads Page</h1>
       <VideoPlayer
-        // src={LostMaryVideo}
-        src="https://res.cloudinary.com/dftbfepcc/video/upload/v1731091258/samples/sea-turtle.mp4"
+        src={LostMaryVideo}
+        // src="https://res.cloudinary.com/dftbfepcc/video/upload/v1731091258/samples/sea-turtle.mp4"
         className="w-[900px] h-[500px] mx-auto" // Tailwind classes for styling
         autoPlay
         muted
