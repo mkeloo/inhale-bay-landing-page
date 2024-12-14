f5f167c687e1724a1a4cbf2493afecb5b6aa1f6f
mkeloo <mkeloo@ufl.edu>
2024-12-14 02:52:42 -0500

    refactor: Comment out unused image imports and export in stuff.ts

--- lib/stuff.ts
+++ lib/stuff.ts
@@ -1,36 +1,36 @@
-import image1 from '@/public/assets/sample_images/image1.webp';
-// import image1 from 'https://asset.gecdesigns.com/img/wallpapers/blue-purple-beautiful-scenery-ultra-hd-wallpaper-4k-sr10012421-1706505497434-cover.webp';
-import image2 from '@/public/assets/sample_images/image2.jpg';
-import image3 from '@/public/assets/sample_images/image3.jpg';
-import image4 from '@/public/assets/sample_images/image4.jpg';
-import image5 from '@/public/assets/sample_images/image5.jpg';
-import image6 from '@/public/assets/sample_images/image6.jpg';
-import image7 from '@/public/assets/sample_images/image7.jpg';
-import image8 from '@/public/assets/sample_images/image8.jpg';
-import image9 from '@/public/assets/sample_images/image9.jpg';
-import image10 from '@/public/assets/sample_images/image10.jpg';
-import image11 from '@/public/assets/sample_images/image11.jpg';
-import image12 from '@/public/assets/sample_images/image12.jpg';
-import image13 from '@/public/assets/sample_images/image13.jpg';
-import image14 from '@/public/assets/sample_images/image14.jpg';
-import image15 from '@/public/assets/sample_images/image15.jpg';
+// import image1 from '@/public/assets/sample_images/image1.webp';
+// // import image1 from 'https://asset.gecdesigns.com/img/wallpapers/blue-purple-beautiful-scenery-ultra-hd-wallpaper-4k-sr10012421-1706505497434-cover.webp';
+// import image2 from '@/public/assets/sample_images/image2.jpg';
+// import image3 from '@/public/assets/sample_images/image3.jpg';
+// import image4 from '@/public/assets/sample_images/image4.jpg';
+// import image5 from '@/public/assets/sample_images/image5.jpg';
+// import image6 from '@/public/assets/sample_images/image6.jpg';
+// import image7 from '@/public/assets/sample_images/image7.jpg';
+// import image8 from '@/public/assets/sample_images/image8.jpg';
+// import image9 from '@/public/assets/sample_images/image9.jpg';
+// import image10 from '@/public/assets/sample_images/image10.jpg';
+// import image11 from '@/public/assets/sample_images/image11.jpg';
+// import image12 from '@/public/assets/sample_images/image12.jpg';
+// import image13 from '@/public/assets/sample_images/image13.jpg';
+// import image14 from '@/public/assets/sample_images/image14.jpg';
+// import image15 from '@/public/assets/sample_images/image15.jpg';
 
 
 
-export const images = [
-    image1,
-    image2,
-    image3,
-    image4,
-    image5,
-    image6,
-    image7,
-    image8,
-    image9,
-    image10,
-    image11,
-    image12,
-    image13,
-    image14,
-    image15,
-];
+// export const images = [
+//     image1,
+//     image2,
+//     image3,
+//     image4,
+//     image5,
+//     image6,
+//     image7,
+//     image8,
+//     image9,
+//     image10,
+//     image11,
+//     image12,
+//     image13,
+//     image14,
+//     image15,
+// ];
