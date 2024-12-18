69da954e44a87284813722789cfe7c870ea3782f
mkeloo <mkeloo@ufl.edu>
2024-12-17 22:22:10 -0500

    feat: Refactor deals data structure to use dynamic image URLs and update IDs for consistency

--- lib/deals.ts
+++ lib/deals.ts
@@ -1,15 +1,9 @@
-import deal1 from "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/geekbar-deals6.png";
-import deal2 from "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/orian-deals3.png";
-import deal3 from "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/raz-deals1.webp";
-import deal4 from "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/southconnect-deals4.png";
-import deal5 from "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/southconnect-deals5.png";
-import deal6 from "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/vtouch-deals2.png";
-
+const bucketBaseUrl = "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/VapesDeals";
 
 
 export const deals = [
     {
-        id: 4,
+        id: 1,
         vape_company: "V Touch",
         base_deal: [
             {
@@ -20,11 +14,11 @@ export const deals = [
         discount_percent: "20",
         deal_tagline: "BUY 1 for $25, GET 2 for $40",
         short_title: "V Touch Mega Deal",
-        imgSrc: "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/southconnect-deals4.png",
+        imgSrc: `${bucketBaseUrl}/1-vtouch.webp`,
         bgGradient: "from-blue-400 to-indigo-900",
     },
     {
-        id: 5,
+        id: 2,
         vape_company: "South Connect",
         base_deal: [
             {
@@ -35,11 +29,11 @@ export const deals = [
         discount_percent: "20",
         deal_tagline: "BUY 1 for $25, GET 2 for $40",
         short_title: "South Connect Offer",
-        imgSrc: "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/southconnect-deals5.png",
+        imgSrc: `${bucketBaseUrl}/2-south-connect.webp`,
         bgGradient: "from-purple-400 to-lime-500",
     },
     {
-        id: 6,
+        id: 3,
         vape_company: "Geek Bar 15K",
         base_deal: [
             {
@@ -50,11 +44,11 @@ export const deals = [
         discount_percent: "17",
         deal_tagline: "BUY 1 for $18, GET 2 for $30",
         short_title: "Geek Bar 15K Promotion",
-        imgSrc: "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/vtouch-deals2.png",
+        imgSrc: `${bucketBaseUrl}/3-geekbar-15k.webp`,
         bgGradient: "from-yellow-400 to-orange-500",
     },
     {
-        id: 7,
+        id: 4,
         vape_company: "Lost Mary 20K",
         base_deal: [
             {
@@ -65,11 +59,11 @@ export const deals = [
         discount_percent: "21",
         deal_tagline: "BUY 1 for $19, GET 2 for $30",
         short_title: "Lost Mary Value Pack",
-        imgSrc: 'https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/geekbar-deals6.png',
+        imgSrc: `${bucketBaseUrl}/4-lost-mary-20k.webp`,
         bgGradient: "from-orange-400 to-yellow-500",
     },
     {
-        id: 8,
+        id: 5,
         vape_company: "Geek Bar 25K",
         base_deal: [
             {
@@ -80,11 +74,11 @@ export const deals = [
         discount_percent: "12.5",
         deal_tagline: "BUY 1 for $20, GET 2 for $35",
         short_title: "Geek Bar 25K Deal",
-        imgSrc: "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/orian-deals3.png",
+        imgSrc: `${bucketBaseUrl}/5-geekbar-25k.webp`,
         bgGradient: "from-purple-400 to-pink-500",
     },
     {
-        id: 9,
+        id: 6,
         vape_company: "Quasar 25K",
         base_deal: [
             {
@@ -95,11 +89,11 @@ export const deals = [
         discount_percent: "12.5",
         deal_tagline: "BUY 1 for $20, GET 2 for $35",
         short_title: "Quasar 25K Savings",
-        imgSrc: "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/raz-deals1.webp",
+        imgSrc: `${bucketBaseUrl}/6-quasar-25k.webp`,
         bgGradient: "from-blue-400 to-lime-500",
     },
     {
-        id: 10,
+        id: 7,
         vape_company: "Fume Extra",
         base_deal: [
             {
@@ -110,26 +104,11 @@ export const deals = [
         discount_percent: "15",
         deal_tagline: "BUY 1 for $10, GET 2 for $17",
         short_title: "Fume Extra Limited Deal",
-        imgSrc: "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/southconnect-deals4.png",
+        imgSrc: `${bucketBaseUrl}/7-fume-extra.webp`,
         bgGradient: "from-blue-400 to-indigo-900",
     },
     {
-        id: 11,
-        vape_company: "Off Stamp 9K with Kit",
-        base_deal: [
-            {
-                buy_1: "$16",
-                buy_2: "$25"
-            }
-        ],
-        discount_percent: "22",
-        deal_tagline: "BUY 1 for $16, GET 2 for $25",
-        short_title: "Off Stamp 9K Kit Discount",
-        imgSrc: "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/southconnect-deals5.png",
-        bgGradient: "from-green-400 to-lime-500",
-    },
-    {
-        id: 12,
+        id: 8,
         vape_company: "Off Stamp 9K No Kit",
         base_deal: [
             {
@@ -140,11 +119,26 @@ export const deals = [
         discount_percent: "15",
         deal_tagline: "BUY 1 for $10, GET 2 for $17",
         short_title: "Off Stamp 9K No Kit Offer",
-        imgSrc: "https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/vtouch-deals2.png",
+        imgSrc: `${bucketBaseUrl}/8-off-stamp-no-kit.webp`,
         bgGradient: "from-green-400 to-orange-500",
     },
     {
-        id: 13,
+        id: 9,
+        vape_company: "Off Stamp 9K with Kit",
+        base_deal: [
+            {
+                buy_1: "$16",
+                buy_2: "$25"
+            }
+        ],
+        discount_percent: "22",
+        deal_tagline: "BUY 1 for $16, GET 2 for $25",
+        short_title: "Off Stamp 9K Kit Discount",
+        imgSrc: `${bucketBaseUrl}/9-off-stamp-kit.webp`,
+        bgGradient: "from-green-400 to-lime-500",
+    },
+    {
+        id: 10,
         vape_company: "Vape Watch",
         base_deal: [
             {
@@ -155,7 +149,7 @@ export const deals = [
         discount_percent: "20",
         deal_tagline: "BUY 1 for $25, GET 2 for $40",
         short_title: "Vape Watch Combo Deal",
-        imgSrc: 'https://pugmboivpnkoasgsgfyf.supabase.co/storage/v1/object/public/store-images/DealsVapesImages/geekbar-deals6.png',
+        imgSrc: `${bucketBaseUrl}/10-vape-watch.webp`,
         bgGradient: "from-orange-400 to-yellow-500",
     }
 ];
