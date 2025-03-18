dcbb0b7b1e744e5969fe578ee043bd56434613f3
mkeloo <mkeloo@ufl.edu>
2025-03-18 00:53:15 -0400

    feat: Add Google Analytics tracking script to RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 // import localFont from 'next/font/local';
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
@@ -29,6 +30,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-P8JFL9HTRG"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             
+             gtag('config', 'G-P8JFL9HTRG');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider
           attribute="class"
@@ -36,7 +53,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-        
+
           {children}
         </ThemeProvider>
       </body>
